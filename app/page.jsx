93de478136b42c0cc7ea6c635409eaddb6ee0808
DSRCF <safian1234123@hotmail.com>
2025-09-14 23:30:58 +0100

    Update page.jsx
    updated main page description

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
             Hello I'm <br /> <span className="text-blue-500">Safian</span>
           </h1>
           <p className="max-w-[500px] mb-9 text-muted-foreground">
-            I excel at crafting elegant digital experiences and I am proficient in a wide range of programming languages and frameworks
+            I build intelligent applications and systems using Python, AI/ML, and modern web technologies. From data-driven insights to interactive digital experiences, I bring ideas to life with practical, scalable solutions
           </p>
           <div className="flex flex-col xl:flex-row items-center gap-8">
             <Button 
